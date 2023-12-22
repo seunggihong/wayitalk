d36ea022ee915f89ed8766c66a57df09b83f1c97
seunggi <hsk1533@naver.com>
2023-12-23 02:17:30 +0900

    change func style

--- utils/LoadData.py
+++ utils/LoadData.py
@@ -9,26 +9,50 @@ load_dotenv()
 DATA_PATH = os.environ.get('DATA_PATH')
 USER_NAME = os.environ.get('USER_NAME')
 
-path = DATA_PATH
-file_list = os.listdir(path)
-file_list_py = [file for file in file_list if file.endswith('.txt')]
+def load_data(data_path, file_format='.txt'):
+    file_list = os.listdir(data_path)
+    file_name_list = [file for file in file_list if file.endswith(file_format)]
+    data = {'datetime': [], 'time': [], 'user': [], 'message': []}
 
-data = {'날짜': [], '시간': [], '발언자': [], '내용': []}
+    for file_name in file_name_list:
+        with open(data_path + file_name, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
 
-for file_name in file_list_py:
-    with open(path + file_name, 'r', encoding='utf-8') as file:
-        lines = file.readlines()
+        for line in lines[1:]:
+            match = re.match(r'(\d{4}\.\s\d{2}\.\s\d{2}\.\s\d{2}:\d{2}),\s(.+?)\s:\s(.+)', line)
+            if match:
+                date_time_str, speaker, content = match.groups()
+                date_time = datetime.strptime(date_time_str, '%Y. %m. %d. %H:%M')
+                data['datetime'].append(date_time.date())
+                data['time'].append(date_time.time())
+                data['user'].append(speaker)
+                data['message'].append(content)
 
-    for line in lines[1:]:
-        match = re.match(r'(\d{4}\.\s\d{2}\.\s\d{2}\.\s\d{2}:\d{2}),\s(.+?)\s:\s(.+)', line)
-        if match:
-            date_time_str, speaker, content = match.groups()
-            date_time = datetime.strptime(date_time_str, '%Y. %m. %d. %H:%M')
-            data['날짜'].append(date_time.date())
-            data['시간'].append(date_time.time())
-            data['발언자'].append(speaker)
-            data['내용'].append(content)
+    return pd.DataFrame(data)
 
-df = pd.DataFrame(data)
+def slice_data(data_frame, user_name):
+    data = {'data':[], 'target':[]}
+    cnt = 0
+    while True:
+        if cnt == (len(df) -1) :
+            break
+        user = data_frame['user'][cnt]
+        message = ''
+        for i in range(cnt, len(data_frame)-1):
+            if user == data_frame['user'][i]:
+                message += (data_frame['message'][i] + " ")
+                cnt += 1
+            else:
+                break
 
-df.to_csv(DATA_PATH + 'talk.csv')
+        if user == user_name:
+            data['target'].append(message)
+        else :
+            data['data'].append(message)
+    
+    return pd.DataFrame(data)
+
+if __name__ == '__main__':
+    df = load_data(DATA_PATH)
+    df = slice_data(df, USER_NAME)
+    print(df.info())
