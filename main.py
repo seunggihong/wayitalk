8c210de6cb31ede15da61acb7f4a1bd5b95a4cec
seunggi <hsk1533@naver.com>
2023-12-26 21:15:51 +0900

    data processing

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import konlpy
+from konlpy.tag import Okt
 from utils.LoadData import slice_data, load_data
 from dotenv import load_dotenv
 import os
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 load_dotenv()
 
@@ -11,4 +13,9 @@ USER_NAME = os.environ.get('USER_NAME')
 df = load_data(DATA_PATH)
 df = slice_data(df, USER_NAME)
 
-print(df.head())
+okt = Okt()
+df['data'] = df['data'].apply(lambda x : re.sub(r'[^ ㄱ-ㅣ가-힣]+', "", x))
+df['target'] = df['target'].apply(lambda x : re.sub(r'[^ ㄱ-ㅣ가-힣]+', "", x))
+
+df['data'] = df['data'].apply(lambda x : okt.se(x))
+df['target'] = df['target'].apply(lambda x : okt.morphs(x))
